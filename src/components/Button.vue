ff5ffb2703d4ea627270dea177c8539d4cc85f17
dezoliveira <andresoliveira@protonmail.com>
2023-03-16 23:15:51 -0300

    FortAwesome Icons

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -7,28 +7,36 @@ export default {
 </script>
 <template>
   <div class="btnFilter">
-    <input type="button" :value="this.value"/>
+    <button type="button">{{this.value}}<i><font-awesome icon="phone"></font-awesome></i></button>
   </div>
 </template>
 <style>
   .btnFilter {
-    width: 200px;
+    width: 100%;
   }
 
-  .btnFilter input {
+  .btnFilter button {
     background: hsl(356, 88%, 24%);
     color: #FFFFF0;
     border-radius: 16px;
     border: 2px solid rgba(15, 14, 14, 0.585);
-    padding: 10px 10px;
+    padding: 0;
     font-size: 20px;
     width: 100%;
     transition: 0.3s all;
+    display: flex;
+    align-items: center;
+    justify-content: center;
   }
 
-  .btnFilter input:hover {
+  .btnFilter button:hover {
     font-weight: bold;
-    padding: 12px;
+    padding: 3px;
     background: hsl(356, 88%, 35%);
+  }
+
+  .btnFilter button i {
+    font-size: 15px;
+    padding: 0;
   }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,4 +3,13 @@ import App from './App.vue'
 
 import './assets/main.css'
 
-createApp(App).mount('#app')
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faPhone } from '@fortawesome/free-solid-svg-icons'
+/* import { far } from '@fortawesome/free-regular-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons' */
+//Add all icons to the library so you can use it in your page
+library.add(faPhone)
+
+createApp(App)
+  .component("font-awesome", FontAwesomeIcon).mount("#app");
