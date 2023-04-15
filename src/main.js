4ce96e4e369236743a2ef5bfc354433c89b82cbc
dezoliveira <andresoliveira@protonmail.com>
2023-04-15 12:51:25 -0300

    another build54

--- src/main.js
+++ src/main.js
@@ -1,16 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
 import './assets/main.css'
 
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+/* import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faPhone, faChartSimple, faTableCellsLarge } from '@fortawesome/free-solid-svg-icons'
 import { } from '@fortawesome/free-regular-svg-icons'
 import { } from '@fortawesome/free-brands-svg-icons'
 //Add all icons to the library so you can use it in your page
-library.add(faPhone, faChartSimple, faTableCellsLarge)
+library.add(faPhone, faChartSimple, faTableCellsLarge) */
 
 createApp(App)
-  .component("font-awesome", FontAwesomeIcon)
+/*   .component("font-awesome", FontAwesomeIcon) */
   .mount("#app");
